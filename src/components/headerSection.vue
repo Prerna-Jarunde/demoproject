246a5d5dcb5578e77814d0db0040dba36ca72d65
HarshadKapei <harshadkapei@gmail.com>
2019-07-19 18:41:20 +0530

    Successfully Learned

---
+++ src/components/headerSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>Demoeeee</h1>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'HeaderSection',
+	props: {
+		msg: String,
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/mainpage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HeaderSection/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderSection from '@/components/headerSection.vue';
+
+export default {
+	name: 'mainpage',
+	components: {
+		HeaderSection,
+	},
+};
+</script>
